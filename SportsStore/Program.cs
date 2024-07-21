999e62699c56b56ff7ef1893b7fccd61df4b9bb9
zilmukhamedoff <askhat.zilmukhamedov@gmail.com>
2024-07-21 12:53:25 -0600

    Add project files.

---
+++ SportsStore/Program.cs
@@ -0,0 +1,15 @@
+namespace SportsStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapDefaultControllerRoute();
+            app.Run();
+        }
+    }
+}
